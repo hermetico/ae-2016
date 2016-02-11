7f7a535c08d378b4dfff7a75fc84f35f19a7e31a
Kim Laursen <laursen86@gmail.com>
2016-02-11 14:53:04 +0100

    Changed format and to work with linux.

--- project-1/BFS/plot.sh
+++ project-1/BFS/plot.sh
@@ -7,11 +7,16 @@ set terminal postscript eps enhanced color
 set logscale x
 set xlabel "N"
 set ylabel "Running time"
+set format y "%.0s*10^%T"
+set yrange [0:0.00000003]
 set output "data/test.eps"
-plot "data/bfs_O3_vector_recursive.data" title "BFS -O3 Recursive", "data/bfs_O3_vector_iterative.data" title "BFS -O3 Iterative"
+#plot "data/bfs_O3_vector_recursive.data" title "BFS -O3 Recursive", \
+#		 "data/bfs_O3_vector_iterative.data" title "BFS -O3 Iterative"
+plot "data/bfs_O3_vector_recursive.data" using (\$1):(\$2/log(\$1)) title "BFS -O3 Recursive", \
+		 "data/bfs_O3_vector_iterative.data" using (\$1):(\$2/log(\$1)) title "BFS -O3 Iterative"
 GPLOT
 
 #rm data/bfs_O3_vector_iterative.data
 #rm data/bfs_O3_vector_recursive.data
 
-open "data/test.eps"
+xdg-open "data/test.eps"
