634366b72e73cc052ef403d5752f175c7e1d1279
Kim Laursen <laursen86@gmail.com>
2016-02-13 15:07:00 +0100

    Refactored code to use long_long instead of long. Also added integration with papi.

--- project-1/BFS/main.cpp
+++ project-1/BFS/main.cpp
@@ -1,57 +1,112 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+#include <papi.h>
+
 
 using namespace std;
 
+void handle_error(int error) {
+    cout << error << endl;
+}
 
-void BuildBFSTreeRecursive(vector<int> &bfs, int min, int max, int index, int *sortedArray, double alpha) {
+void init_papi() {
+    int retval = PAPI_library_init(PAPI_VER_CURRENT);
+    if (retval != PAPI_VER_CURRENT && retval < 0) {
+        printf("PAPI library version mismatch!\n");
+        exit(1);
+    }
+    if (retval < 0) handle_error(retval);
 
-    int size = max-min+1;
-    int pointerIndex = min + size*alpha;
+/*    std::cout << "PAPI Version Number: MAJOR: " << PAPI_VERSION_MAJOR(retval)
+    << " MINOR: " << PAPI_VERSION_MINOR(retval)
+    << " REVISION: " << PAPI_VERSION_REVISION(retval) << "\n";*/
+}
+
+int begin_papi(vector<int> &Events) {
+    int EventSet = PAPI_NULL;
+    int rv;
+    /* Create the Event Set */
+    if ((rv = PAPI_create_eventset(&EventSet)) != PAPI_OK)
+        handle_error(rv);
+
+    for(int i = 0; i < Events.size(); i++) {
+        if ((rv = PAPI_add_event(EventSet, Events[i])) != PAPI_OK)
+            handle_error(rv);
+    }
+
+    /* Start counting events in the Event Set */
+    if ((rv = PAPI_start(EventSet)) != PAPI_OK)
+        handle_error(rv);
+    return EventSet;
+}
+
+void end_papi(int EventSet, long_long *returnValue) {
+    int rv;
+
+    /* get the values */
+    if ((rv = PAPI_stop(EventSet, returnValue)) != PAPI_OK)
+        handle_error(rv);
+
+    /* Remove all events in the eventset */
+    if ((rv = PAPI_cleanup_eventset(EventSet)) != PAPI_OK)
+        handle_error(rv);
+
+    /* Free all memory and data structures, EventSet must be empty. */
+    if ((rv = PAPI_destroy_eventset(&EventSet)) != PAPI_OK)
+        handle_error(rv);
+
+}
+
+void BuildBFSTreeRecursive(vector<long_long> &bfs, long_long min, long_long max, long_long index, vector<long_long> &sortedArray) {
+
+    long_long size = max-min+1;
+    long_long pointerIndex = min + size*0.5;
 
     bfs[index] = sortedArray[pointerIndex];
 
     if(size > 1) {
-        BuildBFSTreeRecursive(bfs, min, pointerIndex - 1, 2 * index + 1, sortedArray, alpha);
-        BuildBFSTreeRecursive(bfs, pointerIndex + 1, max, 2 * index + 2, sortedArray, alpha);
+        BuildBFSTreeRecursive(bfs, min, pointerIndex - 1, 2 * index + 1, sortedArray);
+        BuildBFSTreeRecursive(bfs, pointerIndex + 1, max, 2 * index + 2, sortedArray);
     }
 
 }
 
-void BuildBFSTree(vector<int> &bfs, int *sortedArray, int size, double alpha) {
-    BuildBFSTreeRecursive(bfs, 0, size-1, 0, sortedArray, alpha);
+void BuildBFSTree(vector<long_long> &bfs, vector<long_long> &sortedArray) {
+    BuildBFSTreeRecursive(bfs, 0, sortedArray.size()-1, 0, sortedArray);
 }
 
-void GenerateInOrderArray(int *outputArray, int size) {
-    for(int i=0; i < size; i++) {
+void GenerateInOrderArray(vector<long_long> &outputArray) {
+    for(long_long i=0; i < outputArray.size(); i++) {
         outputArray[i] = i+1;
     }
 }
 
-int BFSSearchRecursive(int query, const vector<int> &tree, int size, int index) {
-    if(index >= size) {
+long_long BFSSearchRecursive(long_long query, const vector<long_long> &tree, long_long index) {
+    if(index >= tree.size()) {
         return -1;
     }
 
     if(query < tree[index]) {
-        return BFSSearchRecursive(query, tree, size, 2*index+1);
+        return BFSSearchRecursive(query, tree, 2*index+1);
     } else if(query > tree[index]) {
-        return BFSSearchRecursive(query, tree, size, 2*index+2);
+        return BFSSearchRecursive(query, tree, 2*index+2);
     }
 
     return query;
 
 }
 
-int BFSSearch(int query, const vector<int> &tree, int size) {
-    return BFSSearchRecursive(query, tree, size, 0);
+long_long BFSSearch(long_long query, const vector<long_long> &tree) {
+    return BFSSearchRecursive(query, tree, 0);
 }
 
-long BFSSearchIterative(int query, const vector<int> &tree, int size) {
+long_long BFSSearchIterative(long_long query, const vector<long_long> &tree) {
 
-    long i = 0;
+    long_long i = 0;
 
-    while(i < size) {
+    while(i < tree.size()) {
 
         if(query == tree[i]) {
             return query;
@@ -71,48 +126,63 @@ long BFSSearchIterative(int query, const vector<int> &tree, int size) {
 
 int main(int argc, char **args) {
 
-    const long min_size = 10;
-    const long max_size = 1000000;
-    const long avg = 100000;
-    const double alpha = 0.5;
-    int result;
+    const long_long min_size = 10;
+    const long_long max_size = 10000000;
+    const long_long avg = 100000;
+
+    vector<int> events;
+    events.push_back(PAPI_BR_MSP);
+    events.push_back(PAPI_L1_DCM);
+    events.push_back(PAPI_L2_DCM);
+    long_long resultValues[events.size()];
+
+    long_long result;
     clock_t begin_t, end_t;
+
+    init_papi();
 
     srand(time(0));
 
-    for (int x = min_size; x <= max_size; x *= 1.1) {
-        int inOrder[x];
-        int maxBfsTreeSize = 2*x;
-        vector<int> bfsTree(maxBfsTreeSize);
-        for(int i=0; i < maxBfsTreeSize; i++) {
-            bfsTree[i] = 0;
-        }
-        int s;
+    for (long_long x = min_size; x <= max_size; x *= 1.1) {
+        vector<long_long> inOrder(x);
+        vector<long_long> bfsTree(2*inOrder.size(), 0);
+        long_long s;
 
-        GenerateInOrderArray(inOrder, x);
-        BuildBFSTree(bfsTree, inOrder, x, alpha);
+        GenerateInOrderArray(inOrder);
+        BuildBFSTree(bfsTree, inOrder);
 
+        int EventSet = begin_papi(events);
         begin_t = clock();
 
-        for (int j = 0; j < avg; j++) {
+        for (long_long j = 0; j < avg; j++) {
             s = 0 + (rand() % ((x * 4) - 0 + 1));
 
             if(argc > 1 && *args[1] == 'i') {
-                result = BFSSearchIterative(s, bfsTree, maxBfsTreeSize);
+                result = BFSSearchIterative(s, bfsTree);
             } else {
-                result = BFSSearch(s, bfsTree, maxBfsTreeSize);
+                result = BFSSearch(s, bfsTree);
             }
         }
 
         end_t = clock();
+        end_papi(EventSet, resultValues);
 
         double elapsed_secs = (double(end_t - begin_t) / CLOCKS_PER_SEC) / avg;
-        cout << x << " " << elapsed_secs << endl;
+
+        cout << x << " " << elapsed_secs;
+
+        for(int i=0; i < events.size(); i++) {
+            double value = resultValues[i] / avg;
+            cout << " " << value;
+        }
+
+        cout << endl;
+
 
     }
 
     //To avoid the BFSSearch to be striped away at -O3 optimization
-    cout << "Dummy: " << result << endl;
+    result = 42;	
 
     return 0;
 }
