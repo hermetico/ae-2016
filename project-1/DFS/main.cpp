bd7b7b2f0ee18e9ea555de5bb706e8692f749179
hermetico <yojmarinv@gmail.com>
2016-02-15 13:02:47 +0100

    included papi enviroment and functions, from BFS ( nice work btw! ). comments: DFS seems to be a little bit slower, it should be something in the implementation, I'll continue checking

--- project-1/DFS/main.cpp
+++ project-1/DFS/main.cpp
@@ -1,12 +1,58 @@
 #include <iostream>
 #include <vector>
 #include "DFSArray.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include <papi.h>
-
 using namespace std;
 
 void handle_error(int error) {
     cout << error << endl;
+}
+
+void init_papi() {
+    int retval = PAPI_library_init(PAPI_VER_CURRENT);
+    if (retval != PAPI_VER_CURRENT && retval < 0) {
+        printf("PAPI library version mismatch!\n");
+        exit(1);
+    }
+    if (retval < 0) handle_error(retval);
+
+}
+
+int begin_papi(vector<int> &Events) {
+    int EventSet = PAPI_NULL;
+    int rv;
+    /* Create the Event Set */
+    if ((rv = PAPI_create_eventset(&EventSet)) != PAPI_OK)
+        handle_error(rv);
+
+    for(int i = 0; i < Events.size(); i++) {
+        if ((rv = PAPI_add_event(EventSet, Events[i])) != PAPI_OK)
+            handle_error(rv);
+    }
+
+    /* Start counting events in the Event Set */
+    if ((rv = PAPI_start(EventSet)) != PAPI_OK)
+        handle_error(rv);
+    return EventSet;
+}
+
+void end_papi(int EventSet, long_long *returnValue) {
+    int rv;
+
+    /* get the values */
+    if ((rv = PAPI_stop(EventSet, returnValue)) != PAPI_OK)
+        handle_error(rv);
+
+    /* Remove all events in the eventset */
+    if ((rv = PAPI_cleanup_eventset(EventSet)) != PAPI_OK)
+        handle_error(rv);
+
+    /* Free all memory and data structures, EventSet must be empty. */
+    if ((rv = PAPI_destroy_eventset(&EventSet)) != PAPI_OK)
+        handle_error(rv);
+
 }
 
 
@@ -70,17 +116,28 @@ void testDFS()
 
 int main() {
 
+    // test params
     const long_long min_size = 10;
     const long_long max_size = 10000000;
     const long_long avg = 100000;
 
+    // tree and data params
     const int init = 1, offset = 10;
     const float alpha = .5;
     DFSArray *tree;
 
+    // papi params
+    vector<int> events;
+    events.push_back(PAPI_BR_MSP);
+    events.push_back(PAPI_L1_DCM);
+    events.push_back(PAPI_L2_DCM);
+    long_long resultValues[events.size()];
+
+
     long_long result;
     clock_t begin_t, end_t;
 
+    init_papi();
     srand(time(0));
     tree = new DFSArray(alpha);
 
@@ -90,6 +147,7 @@ int main() {
         fillRandomData(data, init, offset);
 
         tree->fill(data);
+        int EventSet = begin_papi(events);
         begin_t = clock();
 
         for (long_long j = 0; j < avg; j++) {
@@ -99,10 +157,17 @@ int main() {
         }
 
         end_t = clock();
+        end_papi(EventSet, resultValues);
 
         double elapsed_secs = (double(end_t - begin_t) / CLOCKS_PER_SEC) / avg;
 
         cout << x << " " << elapsed_secs;
+
+        for(int i=0; i < events.size(); i++) {
+            double value = double(resultValues[i]) / avg;
+            cout << " " << value;
+        }
+
 
         cout << endl;
 
