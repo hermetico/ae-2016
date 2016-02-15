3706c9a39b9ce98eb5afc90f98700a3279cbf023
Kim Laursen <laursen86@gmail.com>
2016-02-15 21:09:46 +0100

    Made correct log2 axis.

--- project-1/BFS/plot.sh
+++ project-1/BFS/plot.sh
@@ -6,16 +6,12 @@ gnuplot << GPLOT
 set terminal postscript eps enhanced color
 #set logscale x
 set xlabel "log(n)"
-set ylabel "Running time/log^2(n)"
+set ylabel "Running time"
 set format y "%.0s*10^%T"
-set yrange  [0.00000002:0.00000025]
+#set yrange  [0.00000002:0.00000025]
 set output "$PLOTOUTPUT/BFS_running_time.eps"
 
-plot "$DATADIR/bfs_O3_vector_recursive.data" using (log(\$1)):(\$2) title "BFS g++ with -O3"
-
-#plot "$DATADIR/bfs_O3_vector_recursive.data" using (log(\$1)):(\$2/(log(\$1)*log(\$1))) title "BFS -O3 #Recursive", \
-#     "$DATADIR/bfs_O3_vector_iterative.data" using (log(\$1)):(\$2/(log(\$1)*log(\$1))) title "BFS -O3 Iterative"
-
+plot "$DATADIR/bfs_O3_vector_recursive.data" using (log(\$1)/log(2)):(\$2) title "BFS g++ with -O3"
 GPLOT
 
 
@@ -27,7 +23,7 @@ set ylabel "Mispridictions/log(n)"
 set yrange [0:1.5]
 set output "$PLOTOUTPUT/BFS_branch_mispridictions.eps"
 
-plot "$DATADIR/bfs_O3_vector_recursive.data" using (log(\$1)):(\$3/log(\$1)) title "BFS g++ with -O3"
+plot "$DATADIR/bfs_O3_vector_recursive.data" using (log(\$1)/log(2)):(\$3/(log(\$1)/log(2))) title "BFS g++ with -O3"
 GPLOT
 
 
@@ -40,6 +36,6 @@ set ylabel "Misses"
 #set yrange [0:20]
 set output "$PLOTOUTPUT/BFS_cache_misses.eps"
 
-plot "$DATADIR/bfs_O3_vector_recursive.data" using (log(\$1)):4 title "L1 Cache misses", \
-     "$DATADIR/bfs_O3_vector_recursive.data" using (log(\$1)):5 title "L2 Cache misses"
+plot "$DATADIR/bfs_O3_vector_recursive.data" using (log(\$1)/log(2)):4 title "L1 Cache misses", \
+     "$DATADIR/bfs_O3_vector_recursive.data" using (log(\$1)/log(2)):5 title "L2 Cache misses"
 GPLOT
