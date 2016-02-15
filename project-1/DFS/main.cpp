1290f32171af76b8850220acfb3071cb5d0c4873
hermetico <yojmarinv@gmail.com>
2016-02-15 12:46:51 +0100

    sorted array with static size

--- project-1/DFS/main.cpp
+++ project-1/DFS/main.cpp
@@ -14,11 +14,22 @@ void handle_error(int error) {
 void fillRandomData(vector<int> *data, int length, int init, int offset)
 {
     int min_offset = 0, increase_offset;
-    for(int i = 0, j = 1; i < length; i++,j++)
+    for(int i = 0, j = init; i < length; i++,j++)
     {
         increase_offset = min_offset + (rand() % (offset - 0 + 1));
         j += increase_offset;
         data->push_back(j);
+    }
+}
+
+void fillRandomData(vector<int> *data,  int init, int offset)
+{
+    int min_offset = 0, increase_offset;
+    for(int i = 0, j = init; i < data->size(); i++,j++)
+    {
+        increase_offset = min_offset + (rand() % (offset - 0 + 1));
+        j += increase_offset;
+        data[j];
     }
 }
 
@@ -75,8 +86,8 @@ int main() {
 
     for (long_long x = min_size; x <= max_size; x *= 1.1) {
         long_long s;
-        vector<int> *data = new vector<int>;
-        fillRandomData(data, x , init, offset);
+        vector<int> *data = new vector<int>(x);
+        fillRandomData(data, init, offset);
 
         tree->fill(data);
         begin_t = clock();
