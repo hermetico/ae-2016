83511f5c93d2ffe00a5e64972a5da9bb2d7fc46f
hermetico <yojmarinv@gmail.com>
2016-02-16 16:55:46 +0100

    not working changes

--- project-1/DFS/run.sh
+++ project-1/DFS/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-DATADIR="$1-" $(date '+%d/%m/%Y %H:%M:%S')
+DATADIR=$1
 mkdir $DATADIR
 
 g++ -O3 main.cpp DFSArray.cpp DFSArray.h -std=c++11 -L/usr/local/lib -lpapi -o dfs
-./dfs >  0 $DATADIR/dfs_array_recursive_continuous.data
-./dfs >  2 $DATADIR/dfs_array_recursive_non_continuous.data
+./dfs 0 > $DATADIR/dfs_array_recursive_continuous.data
+./dfs 2 >  $DATADIR/dfs_array_recursive_non_continuous.data
 
