baeb119df5b79470dd6b5fd269bdaa0ba225f71c
Kim Laursen <laursen86@gmail.com>
2016-02-21 18:25:33 +0100

    Changed output files.

--- project-1/plotComparison.sh
+++ project-1/plotComparison.sh
@@ -11,7 +11,7 @@ set xlabel "log(n)"
 set ylabel "Running time"
 set format y "%.0s*10^%T"
 set yrange  [0.00000002:0.00000080]
-set output "$PLOTOUTPUT/comparison_running_time.eps"
+set output "$PLOTOUTPUT/binary_search_comparison_running_time.eps"
 
 plot "$INORDERDATAFILE" using (log(\$1)/log(2)):2 title "InOrder -O3", \
      "$BFSDATAFILE" using (log(\$1)/log(2)):2 title "BFS -O3"
@@ -24,7 +24,7 @@ set terminal postscript eps enhanced color
 set xlabel "log(n)"
 set ylabel "Branche Mispridictions/log(n)"
 set yrange [0:1.5]
-set output "$PLOTOUTPUT/comparison_branch_mispredictions.eps"
+set output "$PLOTOUTPUT/binary_search_comparison_branch_mispredictions.eps"
 
 plot "$INORDERDATAFILE" using (log(\$1)/log(2)):(\$3/(log(\$1)/log(2))) title "InOrder -O3", \
      "$BFSDATAFILE" using (log(\$1)/log(2)):(\$3/(log(\$1)/log(2))) title "BFS -O3"
@@ -37,7 +37,7 @@ set terminal postscript eps enhanced color
 set xlabel "log(n)"
 set ylabel "Cache Misses"
 set yrange [0:45]
-set output "$PLOTOUTPUT/comparison_cache_misses.eps"
+set output "$PLOTOUTPUT/binary_search_comparison_cache_misses.eps"
 
 plot "$INORDERDATAFILE" using (log(\$1)/log(2)):4 title "InOrder -O3: L1 Cache", \
      "$INORDERDATAFILE" using (log(\$1)/log(2)):5 title "InOrder -O3: L2 Cache", \
