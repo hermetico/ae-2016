c7ffbfdeb76a06df293b06ea18e0ece11f6bc39c
hermetico <yojmarinv@gmail.com>
2016-02-25 02:24:20 +0100

    deleted no needed stuff

--- PriorityQueues/main.cpp
+++ PriorityQueues/main.cpp
@@ -9,46 +9,38 @@
 #include "Sanders/shared/util.h" // the heaps util stuff
 #include "Sanders/heap-CLR.h" // the slow heap
 #include "Sanders/heap4.h" // the 4-ary heap
-#include "Sanders/knheap.C"
-//#include "heap4.h"
-//#include "knheap.C"
-
-
-#define rand32() (ran32State = 1664525 * ran32State + 1013904223)
-#define getRandom() ((rand32()), (int)(ran32State >> 2))
+#include "Sanders/knheap.C" // the fast one
 
 using namespace std;
 
 
 void performance_test(IPriorityQueue<int> *heap, int n)
 {
-    Measure measureUnit = Measure();
+
 
     int j;
-    int newElem, k, v;
-    //double insertSum=0, deleteSum=0;
-    static int ran32State = 42 << 20;
-
+    Measure measureUnit = Measure();
     measureUnit.Begin();
+
     // PAPER TESTS
     // insert + delete + add -> from 0 until n, there will be n elements after the loop
-    for (j = 0;  j < n;  j++) {
+    for (j = 0;  j < n;  j++)
+    {
         heap->Insert(j);
         heap->DeleteMin();
         heap->Insert(j+1);
     }
+
     // delete + insert + delete -> from 0 to n, there will be 0 elements after the loop
-    for (j = 0;  j < n;  j++) {
+    for (j = 0;  j < n;  j++)
+    {
         heap->DeleteMin();
         heap->Insert(j);
         heap->DeleteMin();
     }
 
-
-
     measureUnit.End();
-
-    // it gives us the time for each pair of operations
+    // it gives us the time for each pair of operations (Think so)
     measureUnit.Print<int>(n, n);
 
 }
@@ -56,11 +48,6 @@ void performance_test(IPriorityQueue<int> *heap, int n)
 
 int main(int argc, char **argv)
 {
-    // first parameter the number of the elements in the queue
-    // second parameter the number of iterations
-    // third parameter the type of the heap
-    int ran32State = 42 << 20;
-
     int heap_type =  atoi(argv[1]);
     IPriorityQueue<int> *common_heap;
 
