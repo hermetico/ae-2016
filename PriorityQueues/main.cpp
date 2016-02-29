c2d00d83d8bea6e7d6599d36f0e998aaa347511e
Kim Laursen <laursen86@gmail.com>
2016-02-29 19:22:29 +0100

    Updated plots to fit with the article.

--- PriorityQueues/main.cpp
+++ PriorityQueues/main.cpp
@@ -15,12 +15,8 @@
 using namespace std;
 
 
-void performance_test(IPriorityQueue<int> *heap, int n)
-{
-
+void test(IPriorityQueue<int> *heap, int n) {
     int j;
-    Measure measureUnit = Measure();
-    measureUnit.Begin();
 
     // PAPER TESTS
     // insert + delete + add -> from 0 until n, there will be n elements after the loop
@@ -39,40 +35,55 @@ void performance_test(IPriorityQueue<int> *heap, int n)
         heap->DeleteMin();
     }
 
+}
+
+void performance_test(IPriorityQueue<int> *heap, int n, int runs = 1)
+{
+    //Warm up
+    //test(heap, n);
+
+    Measure measureUnit = Measure();
+    measureUnit.Begin();
+
+    for(int i = 0; i <= runs; i++) {
+        test(heap, n);
+    }
+
     measureUnit.End();
+
     // it gives us the time for each pair of operations (Think so)
-    measureUnit.Print<int>(n, n);
+    measureUnit.Print<int>(n, 3*n*runs);
 
 }
 
 int main(int argc, char **argv)
 {
     int heap_type =  atoi(argv[1]);
+    int runs = 1;
     IPriorityQueue<int> *common_heap;
-
 
     // por debajo de 1000 es lento
     for (long x = Utils::min_size; x <= Utils::max_size; x *= 1.1) {
         switch (heap_type) {
             case 0: {
                 VectorHeap<int> vectorHeap = VectorHeap<int>();
-                performance_test(&vectorHeap, x);
+                performance_test(&vectorHeap, x, runs);
                 break;
             }
             case 1: {
                 Heap2<int, int> sanders_simple_heap = Heap2<int, int>(INT_MAX, -INT_MAX, x);
-                performance_test(&sanders_simple_heap, x);
+                performance_test(&sanders_simple_heap, x, runs);
                 break;
             }
 
             case 2: {
                 Heap4<int, int> sanders_heap4 = Heap4<int, int>(INT_MAX, -INT_MAX, x);
-                performance_test(&sanders_heap4, x);
+                performance_test(&sanders_heap4, x, runs);
                 break;
             }
             case 3: {
                KNHeap<int, int> sanders_knheap = KNHeap<int, int>(INT_MAX, -INT_MAX); // doesn't need the capacity
-               performance_test(&sanders_knheap, x);
+               performance_test(&sanders_knheap, x, runs);
                break;
            }
 
