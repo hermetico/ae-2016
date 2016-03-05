8ff11fb33833d2d85a51c7ff3145eceb20a6040e
Kim Laursen <laursen86@gmail.com>
2016-03-05 14:42:14 +0100

    Added plots.

--- PriorityQueues/plots.sh
+++ PriorityQueues/plots.sh
@@ -20,6 +20,24 @@ plot "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):2 title "Binary he
      "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):2 title "Bottom up aligned 4-ary heap",\
      "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):2 title "Kn heap"
 
+GPLOT
+
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+#set logscale x
+set xlabel "log(n)"
+set ylabel "(T(deleteMin) + T(insert))"
+set format y "%.0s*10^%T"
+#set yrange  [0.00000000:0.00000080]
+set output "$PLOTOUTPUT/priority_queue_running_time_with_fitting.eps"
+
+plot "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):2 title "Binary heap",\
+     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):2 title "Bottom up aligned 4-ary heap",\
+     "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):2 title "Kn heap", \
+     "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):(0.625*10**(-8)*log(\$1)/log(2)-0.5*10**(-9)) title "0.625*10^{-8}*log(n)", \
+     "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):(1.15*10**(-8)*log(\$1)/log(2)-80*10**(-9)) title "1.25*10^{-8}*log(n)", \
+     "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):(0.25*10**(-8)*log(\$1)/log(2)+30*10**(-9)) title "0.25*10^{-8}*log(n)"
+
      #"$DATADIR/sanders_simple_heap_test.data" using (log(\$1)/log(2)):2 title "Bottom up Binary heap ",\
 
 GPLOT
@@ -101,7 +119,7 @@ set ylabel "Branch Mispridictions"
 set yrange [0:5]
 set output "$PLOTOUTPUT/priority_queue_BinaryHeap_branch_mispridictions.eps"
 
-plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):3 title "Binary heap", \
+plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):3 title "Binary heap"
 
      #"$DATADIR/sanders_simple_heap_test.data" using (log(\$1)/log(2)):(\$3)/(log(\$1)/log(2)) title  "Bottom up Binary heap", \
 
