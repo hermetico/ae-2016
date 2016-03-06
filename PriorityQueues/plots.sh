b19d0f57d2c8ed8ca36973f91b583f0a6c575846
Kim Laursen <laursen86@gmail.com>
2016-03-06 16:52:01 +0100

    Changes to plots.

--- PriorityQueues/plots.sh
+++ PriorityQueues/plots.sh
@@ -16,9 +16,9 @@ set format y "%.0s*10^%T"
 #set yrange  [0.00000000:0.00000080]
 set output "$PLOTOUTPUT/priority_queue_running_time.eps"
 
-plot "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):2 title "Binary heap",\
-     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):2 title "Bottom up aligned 4-ary heap",\
-     "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):2 title "Kn heap"
+plot "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):2 title "Binary Heap",\
+     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):2 title "Bottom up aligned 4-ary Heap",\
+     "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):2 title "KN Heap"
 
 GPLOT
 
@@ -28,17 +28,41 @@ set terminal postscript eps enhanced color
 set xlabel "log(n)"
 set ylabel "(T(deleteMin) + T(insert))"
 set format y "%.0s*10^%T"
-#set yrange  [0.00000000:0.00000080]
-set output "$PLOTOUTPUT/priority_queue_running_time_with_fitting.eps"
+set yrange  [0.00000000:0.00000035]
+set output "$PLOTOUTPUT/priority_queue_running_time_simple_heap_with_fitting.eps"
 
-plot "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):2 title "Binary heap",\
-     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):2 title "Bottom up aligned 4-ary heap",\
-     "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):2 title "Kn heap", \
-     "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):(0.625*10**(-8)*log(\$1)/log(2)-0.5*10**(-9)) title "0.625*10^{-8}*log(n)", \
-     "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):(1.15*10**(-8)*log(\$1)/log(2)-80*10**(-9)) title "1.25*10^{-8}*log(n)", \
-     "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):(0.25*10**(-8)*log(\$1)/log(2)+30*10**(-9)) title "0.25*10^{-8}*log(n)"
+plot "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):2 title "Binary Heap",\
+     "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):(6.25*10**(-9)*log(\$1)/log(2)-0.5*10**(-9)) title "6.25*10^{-9}*log(n)", \
+     "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):(11.5*10**(-9)*log(\$1)/log(2)-80*10**(-9)) title "12.5*10^{-9}*log(n)"
 
-     #"$DATADIR/sanders_simple_heap_test.data" using (log(\$1)/log(2)):2 title "Bottom up Binary heap ",\
+GPLOT
+
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+#set logscale x
+set xlabel "log(n)"
+set ylabel "(T(deleteMin) + T(insert))"
+set format y "%.0s*10^%T"
+set yrange  [0.00000000:0.00000025]
+set output "$PLOTOUTPUT/priority_queue_running_time_sanders_heap4_with_fitting.eps"
+
+plot "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):2 title "Bottom up aligned 4-ary Heap",\
+     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):(4.0*10**(-9)*log(\$1)/log(2)+15*10**(-9)) title "4*10^{-9}*log(n)", \
+     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):(7.2*10**(-9)*log(\$1)/log(2)-3.5*10**(-8)) title "7.2*10^{-9}*log(n)"
+
+GPLOT
+
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+#set logscale x
+set xlabel "log(n)"
+set ylabel "(T(deleteMin) + T(insert))"
+set format y "%.0s*10^%T"
+set yrange  [0.00000000:0.00000025]
+set output "$PLOTOUTPUT/priority_queue_running_time_sanders_knheap_with_fitting.eps"
+
+plot "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):2 title "Kn Heap", \
+     "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):(2.5*10**(-9)*log(\$1)/log(2)+30*10**(-9)) title "2.5*10^{-9}*log(n)"
 
 GPLOT
 
@@ -50,9 +74,9 @@ set ylabel "Branch Mispridictions/log(n)"
 set yrange [0:0.8]
 set output "$PLOTOUTPUT/priority_queue_branch_mispridictions.eps"
 
-plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):(\$3)/(log(\$1)/log(2)) title "Binary heap", \
-     "$DATADIR/sanders_heap4_test.data" using  (log(\$1)/log(2)):(\$3)/(log(\$1)/log(2)) title  "Bottom up aligned 4-ary heap",\
-     "$DATADIR/sanders_knheap_test.data" using  (log(\$1)/log(2)):(\$3)/(log(\$1)/log(2)) title  "Kn heap"
+plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):(\$3)/(log(\$1)/log(2)) title "Binary Heap", \
+     "$DATADIR/sanders_heap4_test.data" using  (log(\$1)/log(2)):(\$3)/(log(\$1)/log(2)) title  "Bottom up aligned 4-ary Heap",\
+     "$DATADIR/sanders_knheap_test.data" using  (log(\$1)/log(2)):(\$3)/(log(\$1)/log(2)) title  "KN Heap"
 
      #"$DATADIR/sanders_simple_heap_test.data" using (log(\$1)/log(2)):(\$3)/(log(\$1)/log(2)) title  "Bottom up Binary heap", \
 
@@ -67,9 +91,9 @@ set ylabel "Level 1 Cache Misses"
 #set yrange [0:50]
 set output "$PLOTOUTPUT/priority_queue_cache_misses_level_1.eps"
 
-plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):4 title "Binary heap", \
-     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):4 title "Bottom up aligned 4-ary heap",\
-     "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):4 title "Kn heap"
+plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):4 title "Binary Heap", \
+     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):4 title "Bottom up aligned 4-ary Heap",\
+     "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):4 title "KN Heap"
      #"$DATADIR/sanders_simple_heap_test.data" using  (log(\$1)/log(2)):4 title "Bottom up Binary heap", \
 
 GPLOT
@@ -84,9 +108,9 @@ set ylabel "L2 Cache Misses"
 #set yrange [0:50]
 set output "$PLOTOUTPUT/priority_queue_cache_misses_level_2.eps"
 
-plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):5 title "Binary heap", \
-     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):5 title "Bottom up aligned 4-ary heap", \
-     "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):5 title "Kn heap"
+plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):5 title "Binary Heap", \
+     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):5 title "Bottom up aligned 4-ary Heap", \
+     "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):5 title "KN Heap"
 
      #"$DATADIR/sanders_simple_heap_test.data" using  (log(\$1)/log(2)):5 title "Bottom up Binary heap", \
 
@@ -119,7 +143,7 @@ set ylabel "Branch Mispridictions"
 set yrange [0:5]
 set output "$PLOTOUTPUT/priority_queue_BinaryHeap_branch_mispridictions.eps"
 
-plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):3 title "Binary heap"
+plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):3 title "Binary Heap"
 
      #"$DATADIR/sanders_simple_heap_test.data" using (log(\$1)/log(2)):(\$3)/(log(\$1)/log(2)) title  "Bottom up Binary heap", \
 
@@ -137,7 +161,7 @@ set format y "%.0s*10^%T"
 #set yrange  [0.00000000:0.00000080]
 set output "$PLOTOUTPUT/kn_heap_running_time.eps"
 
-plot "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):2 title "Kn heap"
+plot "$DATADIR/sanders_knheap_test.data" using (log(\$1)/log(2)):2 title "KN Heap"
 GPLOT
 
 gnuplot << GPLOT
@@ -163,7 +187,7 @@ set yrange [0:5]
 #set xrange [14:24]
 set output "$PLOTOUTPUT/kn_heap_cache_misses_l1.eps"
 
-plot "$DATADIR/sanders_knheap_test.data" using  (log(\$1)/log(2)):(\$4) title "Kn heap" with linespoints, \
+plot "$DATADIR/sanders_knheap_test.data" using  (log(\$1)/log(2)):(\$4) title "KN Heap" with linespoints, \
      "$DATADIR/sanders_knheap_test.data" using  (log(\$1)/log(2)):(0.015 * (log(\$1)/log(2)) ) title "0.015 * log(n)" with linespoints
 GPLOT
 
@@ -176,7 +200,7 @@ set yrange [-0.5:2]
 #set xrange [14:24]
 set output "$PLOTOUTPUT/kn_heap_cache_misses_l2.eps"
 
-plot    "$DATADIR/sanders_knheap_test.data" using  (log(\$1)/log(2)):(\$5) title "Kn heap" with linespoints, \
+plot    "$DATADIR/sanders_knheap_test.data" using  (log(\$1)/log(2)):(\$5) title "KN Heap" with linespoints, \
         "$DATADIR/sanders_knheap_test.data" using  (log(\$1)/log(2)):((0.01 * log(\$1)/log(2)) -0.1 ) title "0.01 * log(n)" with linespoints
 GPLOT
 
