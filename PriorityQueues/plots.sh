f5740c472f523f08b6298afdfe00433e840f62fe
Kim Laursen <laursen86@gmail.com>
2016-03-09 20:41:14 +0100

    Updated plots.

--- PriorityQueues/plots.sh
+++ PriorityQueues/plots.sh
@@ -26,36 +26,6 @@ gnuplot << GPLOT
 set terminal postscript eps enhanced color
 #set logscale x
 set xlabel "log(n)"
-set ylabel "(T(deleteMin) + T(insert))"
-set format y "%.0s*10^%T"
-set yrange  [0.00000000:0.00000035]
-set output "$PLOTOUTPUT/priority_queue_running_time_simple_heap_with_fitting.eps"
-
-plot "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):2 title "Binary Heap",\
-     "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):(6.25*10**(-9)*log(\$1)/log(2)-0.5*10**(-9)) title "6.25*10^{-9}*log(n)" with linespoints, \
-     "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):(11.5*10**(-9)*log(\$1)/log(2)-80*10**(-9)) title "12.5*10^{-9}*log(n)" with linespoints
-
-GPLOT
-
-gnuplot << GPLOT
-set terminal postscript eps enhanced color
-#set logscale x
-set xlabel "log(n)"
-set ylabel "(T(deleteMin) + T(insert))"
-set format y "%.0s*10^%T"
-set yrange  [0.00000000:0.00000025]
-set output "$PLOTOUTPUT/priority_queue_running_time_sanders_heap4_with_fitting.eps"
-
-plot "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):2 title "Bottom up aligned 4-ary Heap",\
-     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):(4.0*10**(-9)*log(\$1)/log(2)+15*10**(-9)) title "4*10^{-9}*log(n)"  with linespoints, \
-     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):(7.2*10**(-9)*log(\$1)/log(2)-3.5*10**(-8)) title "7.2*10^{-9}*log(n)" with linespoints
-
-GPLOT
-
-gnuplot << GPLOT
-set terminal postscript eps enhanced color
-#set logscale x
-set xlabel "log(n)"
 set ylabel "Branch Mispridictions/log(n)"
 set yrange [0:0.8]
 set output "$PLOTOUTPUT/priority_queue_branch_mispridictions.eps"
@@ -105,6 +75,22 @@ GPLOT
 
 ############ Binary Heap #################
 
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+#set logscale x
+set xlabel "log(n)"
+set ylabel "(T(deleteMin) + T(insert))"
+set format y "%.0s*10^%T"
+set yrange  [0.00000000:0.00000035]
+set output "$PLOTOUTPUT/priority_queue_running_time_simple_heap_with_fitting.eps"
+
+plot "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):2 title "Binary Heap",\
+     "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):(6.25*10**(-9)*log(\$1)/log(2)-0.5*10**(-9)) title "6.25*10^{-9}*log(n)" with linespoints, \
+     "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):(11.5*10**(-9)*log(\$1)/log(2)-80*10**(-9)) title "12.5*10^{-9}*log(n)" with linespoints, \
+     "$DATADIR/simple_heap_test.data" using (log(\$1)/log(2)):(47*10**(-9)*log(\$1)/log(2)-805*10**(-9)) title "47*10^{-9}*log(n)" with linespoints
+
+
+GPLOT
 
 gnuplot << GPLOT
 set terminal postscript eps enhanced color
@@ -118,7 +104,9 @@ set output "$PLOTOUTPUT/priority_queue_BinaryHeap_cache_misses.eps"
 plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):(\$4) title "Cache Level 1", \
      "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):(0.75*log(\$1)/log(2)-9.5) title "0.75*log(n)" with linespoints, \
      "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):(\$5) title "Cache Level 2", \
+     "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):(0.75*log(\$1)/log(2)-11.7) title "0.75*log(n)" with linespoints, \
      "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):(0.75*log(\$1)/log(2)-11.7) title "0.75*log(n)" with linespoints
+
 GPLOT
 
 gnuplot << GPLOT
@@ -134,6 +122,27 @@ plot "$DATADIR/simple_heap_test.data" using  (log(\$1)/log(2)):3 title "Binary H
      #"$DATADIR/sanders_simple_heap_test.data" using (log(\$1)/log(2)):(\$3)/(log(\$1)/log(2)) title  "Bottom up Binary heap", \
 
 GPLOT
+
+
+############ 4-ary Heap #################
+
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+#set logscale x
+set xlabel "log(n)"
+set ylabel "(T(deleteMin) + T(insert))"
+set format y "%.0s*10^%T"
+set yrange  [0.00000000:0.00000025]
+set output "$PLOTOUTPUT/priority_queue_running_time_sanders_heap4_with_fitting.eps"
+
+plot "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):2 title "Bottom up aligned 4-ary Heap",\
+     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):(4.0*10**(-9)*log(\$1)/log(2)+15*10**(-9)) title "4*10^{-9}*log(n)"  with linespoints, \
+     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):(7.2*10**(-9)*log(\$1)/log(2)-3.5*10**(-8)) title "7.2*10^{-9}*log(n)" with linespoints, \
+     "$DATADIR/sanders_heap4_test.data" using (log(\$1)/log(2)):(25*10**(-9)*log(\$1)/log(2)-40*10**(-8)) title "25*10^{-9}*log(n)" with linespoints
+
+
+GPLOT
+
 
 
 ############ KN Heap #################
--- project-1/DFS/plots.sh
+++ project-1/DFS/plots.sh
@@ -16,6 +16,21 @@ set output "$PLOTOUTPUT/DFS_running_time.eps"
 plot "$DATADIR/dfs_array_recursive_continuous.data" using (log(\$1)/log(2)):(\$2) title "DFS, alpha = 0.75"
 GPLOT
 
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+#set logscale x
+set xlabel "log(n)"
+set ylabel "Running time"
+set format y "%.0s*10^%T"
+set yrange  [0.00000000:0.00000080]
+set output "$PLOTOUTPUT/DFS_running_time_with_fittings.eps"
+
+plot "$DATADIR/dfs_array_recursive_continuous.data" using (log(\$1)/log(2)):(\$2) title "DFS, alpha = 0.75", \
+     "$DATADIR/dfs_array_recursive_continuous.data" using (log(\$1)/log(2)):(8*10**(-9)*log(\$1)/log(2)+2*10**(-9)) title "8*10^{-9}*log(n)" with linespoints, \
+		 "$DATADIR/dfs_array_recursive_continuous.data" using (log(\$1)/log(2)):(34*10**(-9)*log(\$1)/log(2)-50*10**(-8)) title "34*10^{-9}*log(n)" with linespoints
+
+GPLOT
+
 
 gnuplot << GPLOT
 set terminal postscript eps enhanced color
@@ -39,5 +54,20 @@ set output "$PLOTOUTPUT/DFS_cache_misses.eps"
 
 plot "$DATADIR/dfs_array_recursive_continuous.data" using (log(\$1)/log(2)):4 title "L1 Cache", \
      "$DATADIR/dfs_array_recursive_continuous.data" using (log(\$1)/log(2)):5 title "L2 Cache"
+GPLOT
+
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+#set logscale x
+set xlabel "log(n)"
+set ylabel "Cache Misses"
+set yrange [0:50]
+set output "$PLOTOUTPUT/DFS_cache_misses_with_fittings.eps"
+
+plot "$DATADIR/dfs_array_recursive_continuous.data" using (log(\$1)/log(2)):4 title "L1 Cache", \
+     "$DATADIR/dfs_array_recursive_continuous.data" using (log(\$1)/log(2)):(1.4*log(\$1)/log(2)-20) title "1.4*log(n)" with linespoints, \
+     "$DATADIR/dfs_array_recursive_continuous.data" using (log(\$1)/log(2)):5 title "L2 Cache", \
+     "$DATADIR/dfs_array_recursive_continuous.data" using (log(\$1)/log(2)):(1.3*log(\$1)/log(2)-15) title "1.3*log(n)" with linespoints
+
 GPLOT
 
