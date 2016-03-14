e9179a352564ccf6785fafeb1cc0e88ff4f59115
hermetico <yojmarinv@gmail.com>
2016-03-14 11:19:21 +0100

    data

--- MatrixMult/methods/recmult.h
+++ MatrixMult/methods/recmult.h
@@ -11,7 +11,7 @@ using namespace std;
 class RecMult: public IMatrixMult{
 
 private:
-    int perform_simple_mult_at_level = 52; //0 doesn't perform optimization
+    int perform_simple_mult_at_level = 0; //0 doesn't perform optimization
 
     void simple_mult(int n, int *A, int *B, int *C){
 
